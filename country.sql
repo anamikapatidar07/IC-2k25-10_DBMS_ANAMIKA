8a450830701eac8a6217c6dc188796f29b94dead
anamikapatidar07 <anamikapatidar201107@gmail.com>
2026-08-11 12:42:18 +0530

    Update country.sql

--- country.sql
+++ country.sql
@@ -31,5 +31,65 @@ OUTPUT
 
 
 
+DROP TABLE IF EXISTS countries;
+DROP TABLE IF EXISTS dup_countries;
+
+CREATE TABLE countries (
+    country_id INT PRIMARY KEY,
+    country_name VARCHAR(100) NOT NULL,
+    region_id INT
+);
+
+INSERT INTO countries (country_id, country_name, region_id) VALUES
+(1, 'United States', 101),
+(2, 'United Kingdom', 102),
+(3, 'India', 103),
+(4, 'Australia', 104),
+(5, 'Brazil', 105);
+
+
+CREATE TABLE dup_countries LIKE countries;
+
+INSERT INTO dup_countries (country_id, country_name, region_id) VALUES
+(1, 'Canada', 201),
+(2, 'Japan', 202),
+(3, 'Germany', 203);
+
+SELECT * FROM countries;
+SELECT * FROM dup_countries;
+
+OUTPUT
++------------+----------------+-----------+
+| country_id | country_name   | region_id |
++------------+----------------+-----------+
+|          1 | United States  |       101 |
+|          2 | United Kingdom |       102 |
+|          3 | India          |       103 |
+|          4 | Australia      |       104 |
+|          5 | Brazil         |       105 |
++------------+----------------+-----------+
++------------+--------------+-----------+
+| country_id | country_name | region_id |
++------------+--------------+-----------+
+|          1 | Canada       |       201 |
+|          2 | Japan        |       202 |
+|          3 | Germany      |       203 |
++------------+--------------+-----------+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
