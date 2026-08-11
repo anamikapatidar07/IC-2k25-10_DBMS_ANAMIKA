dfe72e6627402754380f8ec30515f6c089f6c45b
anamikapatidar07 <anamikapatidar201107@gmail.com>
2026-08-11 12:20:12 +0530

    Create it exists.sql

---
+++ it exists.sql
@@ -0,0 +1,39 @@
+
+DROP TABLE IF EXISTS countries;
+
+-- 1. Create the table structure
+CREATE TABLE countries (
+    country_id INT PRIMARY KEY,
+    country_name VARCHAR(100) NOT NULL,
+    region_id INT
+);
+
+-- 2. Insert sample data into the table
+INSERT INTO countries (country_id, country_name, region_id) VALUES
+(1, 'India', 103),
+(2, 'United Kingdom', 102),
+(3, 'United States', 101),
+(4, 'Australia', 104),
+(5, 'Brazil', 105);
+
+-- 3. Select the data to get the final OUTPUT
+SELECT * FROM countries;
+
+OUTPUT
++------------+----------------+-----------+
+| country_id | country_name   | region_id |
++------------+----------------+-----------+
+|          1 | India          |       103 |
+|          2 | United Kingdom |       102 |
+|          3 | United States  |       101 |
+|          4 | Australia      |       104 |
+|          5 | Brazil         |       105 |
++------------+----------------+-----------+
+
+
+
+
+
+
+
+
